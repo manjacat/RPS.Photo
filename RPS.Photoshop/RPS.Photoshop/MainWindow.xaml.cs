fcdc9e5ffd9cd97ad6232c75836ea07921f4dbf4
Khairil Rizal <manjacat@gmail.com>
2020-02-27 15:00:22 +0800

    First code
    First code for RPS.Photoshop

---
+++ RPS.Photoshop/RPS.Photoshop/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace RPS.Photoshop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnStart.IsEnabled = true;
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            lblMessage.Text = "Button is clicked";
+        }
+    }
+}
